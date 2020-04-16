02c658047b2ba1bce737b24bb2eee5bec904df2d
Philip Snow <philipsnow@philips-mbp.home>
2020-04-16 11:53:55 +0200

    sample page work

--- pages/_library/_slug.vue
+++ pages/_library/_slug.vue
@@ -1,86 +1,42 @@
 <template>
-	<div>
-		<h1>{{ slice.fieldset }} from the {{ library.packageName }} library.</h1>
-		<p>{{ slice.description }}</p>
-		<!-- <nuxt-link :to="`/examples/nuxt/${slice.displayName}`">
-			<img class="sample-image" :src="`/components/${slice.displayName}.png`" />
-		</nuxt-link>-->
-		<!-- <div
-			class="card"
-			@mouseover="mouseover"
-			@mouseleave="mouseleave"
-			@click="$router.push(`/examples/nuxt/${slice.displayName}`)"
-		>-->
-		<!-- <div
-				class="image-block"
-				:style="{
-					backgroundImage: `url('/components/${slice.displayName}.png')`
-				}"
-		/>
-		<img class="image-block" :src="`/components/${slice.displayName}.png`" />
-			<div :class="
-					`hover-block ${hover || isInMyList ? 'hover-block--hovered' : ''}`
-				">
-				<h5>View Demo</h5>
-			</div>
-		</div>-->
+	<div class="content">
+		<h1>{{ slice[1].fieldset }} from the {{ library.packageName }} library.</h1>
+		<p>{{ slice[1].description }}</p>
+		<h2>Example</h2>
+		<iframe :src="`https://prismic-sm.netlify.app/components/preview/${example}.html`"></iframe>
 		<prismic-rich-text :field="document.install_info_title" />
 		<prismic-rich-text :field="document.install_info_text" />
-		<!-- <div class="clipboard" @click.stop.prevent="copyCommand">
-			<p class="embed-text">import {{ slice.displayName }}</p>
-			<img src="../../static/clipboard.svg" />
-			<input id="command-to-copy" type="hidden" :value="`import ${slice.displayName}`" />
-		</div>-->
 		<prismic-rich-text :field="document.info_tagline" />
-		<!-- <component :is="slice.slice_type" :data= /> -->
 		<MarkDownBox
 			id="markdown-box"
 			:edit-url="createEditUrl()"
 			style="min-height: 80vh; margin-top: 4em; word-spacing: 0px;"
 		>{{ readme }}</MarkDownBox>
-		<!-- <div v-if="hasSandbox" style="width: 100%; margin-top: 3em;" v-html="sandbox" /> -->
 	</div>
 </template>
 
 <script>
-// import * as Slices from '@/../src/slices'
-// import { createSlice, sliceRoute } from '~/utils'
 
 import MarkDownBox from '@/components/MarkDownBox'
-
-// const lst = Object.keys(Slices)
-// 	.filter(e => e !== 'SliceZone')
-// 	.map(createSlice)
-// 	.filter(e => e) // eslint-disable-line
 
 // import { pascalize, hyphenate } from 'sm-commons/methods/misc'
 
 const camelizeRE = /-(\w)/g
-function pascalize(str) {
-	if (!str) {
-		return ''
-	}
-	str = str.replace(/_/g, '-').replace(camelizeRE, (_, c) => {
-		return c ? c.toUpperCase() : ''
-	})
-	return str[0].toUpperCase() + str.slice(1)
-}
 
 const hyphenateRE = /\B([A-Z])/g
 function hyphenate(str, kebab) {
 	const s = str.replace(hyphenateRE, '-$1').toLowerCase()
 	return kebab ? s.replace(/-/g, '_') : s
 }
+function pascalize(str, kebab) {
+	const s = str.replace(hyphenateRE, '-$1').toLowerCase()
+	return kebab ? s.replace(/-/g, '-') : s
+}
 export default {
-	layout: 'complib',
+	layout: 'docs',
 	components: {
-		// ...Slices,
 		MarkDownBox
-		// component
 	},
-	// async fetch({ store }) {
-	// 	await store.dispatch('slices/init')
-	// },
 	async asyncData({ $prismic, $axios, params, error }) {
 		try {
 			const document = (await $prismic.api.getSingle('sample_pages')).data
@@ -89,20 +45,21 @@ export default {
 				`http://sm-api.now.sh/api/library?lib=${params.library}`
 			)
 
-			const result = Object.getOwnPropertyNames(library.slices).filter(
-				slice => slice === hyphenate(params.slug, true)
+			const result = Object.entries(library.slices).filter(
+				([key, value]) => key === hyphenate(params.slug, true)
 			)
+
+			const example = pascalize(params.slug, true)
 
 			const readme = await $axios.$get(
 				`https://raw.githubusercontent.com/prismicio/${params.library}/master/src/slices/${params.slug}/README.md`
 			)
 
 			return {
-				// Page content
 				document,
 				library,
 				slice: result[0],
-				// component,
+				example,
 				readme
 			}
 		} catch (e) {
@@ -112,23 +69,6 @@ export default {
 			error({ statusCode: 404, message: 'Page not found' })
 		}
 	},
-	// data() {
-	// 	return {
-	// 		document: this.document,
-	// 		slices,
-	// 		// lst,
-	// 		// slice: createSlice(this.$route.params.slug),
-	// 		// exampleFolder: `${sliceRoute(this.$route.params.slug)}/examples/nuxt/`,
-	// 		hover: false
-	// 		// readme
-	// 	}
-	// },
-	// computed: {
-	// 	hasSandbox: ({ slice, $router }) =>
-	// 		$router.resolve({ name: `examples-${slice.displayName}` }).href !== '/',
-	// 	sandbox: ({ slice: { displayName } }) =>
-	// 		`<iframe src="https://codesandbox.io/embed/github/hypervillain/community/tree/master/?autoresize=1&fontsize=14&initialpath=%2Fexamples%2F${displayName}&module=%2Fwebsite%2Fpages%2Fexamples%2F${displayName}.vue&moduleview=0" title="community" allow="geolocation; microphone; camera; midi; vr; accelerometer; gyroscope; payment; ambient-light-sensor; encrypted-media" style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;" sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"></iframe>`
-	// },
 	methods: {
 		mouseover() {
 			this.hover = true
@@ -159,8 +99,6 @@ export default {
 			window.getSelection().removeAllRanges()
 		}
 	}
-	// validate: ({ params: { slug } }) =>
-	// 	process.env.NODE_ENV === 'production' ? createSlice(slug) !== null : true
 }
 </script>
 
@@ -247,5 +185,10 @@ export default {
 	&:hover {
 		background-color: $black-secondary;
 	}
+}
+iframe{
+	width: 100%;
+	height: 350px;
+	border: none;
 }
 </style>
