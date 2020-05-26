9818c3f9147f922dbf7f6ac8d9aa9bb55c4121f7
Anthony Aubertin <anthony.aubertin@gmail.com>
2020-05-26 11:17:53 +0200

    add footer style

--- components/menus/FooterMenu.vue
+++ components/menus/FooterMenu.vue
@@ -1,26 +1,19 @@
 <template>
 	<div :class="`footerMenu ${theme}`">
-		<Container class="foor-container" justify="space-between">
-			<div class="logo">
-				<nuxt-link to="/">
+		<Container class="foot-container" justify="space-between">
+			<div>
+				<div class="logo">
 					<prismic-image v-if="theme == 'light' && menu.logo_black" :field="menu.logo_black" />
 					<prismic-image v-if="theme == 'dark' && menu.logo" :field="menu.logo" />
-				</nuxt-link>
-				<nuxt-link to="/">
-					<span>
-					{{ menu.title_tag }}
-					</span>
-				</nuxt-link>
+					<span>{{ menu.title_tag }}</span>
+				</div>
+				<span class="adress">{{ menu.address }}</span>
 			</div>
-			<nuxt-link to="/">
-				<span>
-				{{ menu.address }}
-				</span>
-			</nuxt-link>
+
 			<nav>
 				<ul v-for="(column, index) in menu.body" :key="'column-' + index">
 					<li class="prominent">
-						<prismic-link :field="column.primary.prominent_link">{{ column.primary.prominent_link_label }}</prismic-link>
+						{{ column.primary.prominent_link_label }}
 					</li>
 					<li class="secondary" v-for="menuLink in column.items" :key="menuLink.id">
 						<prismic-link :field="menuLink.secondary_link">{{ menuLink.secondary_link_label }}</prismic-link>
@@ -70,6 +63,7 @@ a {
 .footerMenu {
 
 	&.dark {
+		color: #FFFFFF;
 		background-color: $black-primary;
 		border-top: 1px solid $grey-primary;
 		a {
@@ -96,10 +90,15 @@ a {
 			}
 		}
 	}
-
-	padding: 30px 0;
+	padding: 30px 0 100px 0;
 	min-height: 80px;
-	.foor-container {
+
+	.adress{
+		display: block;
+		font-size: 14px;
+		opacity: 0.5;
+	}
+	.foot-container {
 		display: block;
 		@include md {
 			display: flex;
@@ -108,24 +107,46 @@ a {
 	.logo {
 		display: inline-flex;
 		align-items: center;
+		img{
+			width: 22px;
+			margin-right: 5px;
+		}
 		b {
 			font-size: 20px;
 			padding: 0 5px;
 		}
 	}
-	ul {
-		display: grid;
-		padding: 0;
-		font-size: 14px;
-		@include md {
-			display: inline-flex;
+	nav{
+		ul {
+			float: left;
+			padding: 0;
+			font-size: 14px;
+			margin-left: 150px;
+			li {
+				display: block;
+				padding-top: 15px;
+				@include md {
+					margin-left: 10px;
+				}
+				&.prominent{
+					font-weight: 600;
+					opacity: 0.5;
+				}
+				&.secondary{}
+			}
 		}
-	}
-	li {
-		display: inline-block;
-		padding-top: 15px;
 		@include md {
-			margin-left: 10px;
+			ul{
+				margin-left: 50px;
+			}
+		}
+		@include sm {
+			border-top: 1px solid #333;
+			margin-top: 20px;
+			ul{
+				float: none;
+				margin-left: 0px;
+			}
 		}
 	}
 	a {
